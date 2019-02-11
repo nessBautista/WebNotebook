999b06163d432b7ffb43fd59195c234ac2c700e5
Nestor Hernandez <nestor.jbautista@gmail.com>
2019-02-11 11:44:44 -0600

    added example of progressive app

--- React101/react-is-fun/src/index.js
+++ React101/react-is-fun/src/index.js
@@ -1,124 +1,36 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-let bookList = [
-  {"title":"The Art of War", "author":"Tzun Zu", pages: 120},
-  {"title":"The call of ktuhulu", "author":"HP Lovecraft", pages: 500},
-  {"title":"The prince", "author":"Maquivelo", pages: 260},
-  {"title":"The gray", "author":"Mark Maks", pages: 260}
-]
-
-const Book = ({title, author, pages}) => {
-  return(
-    <section>
-      <h2>{title}</h2>
-      <p>by: {author}</p>
-      <p>Pages: {pages}</p>
-    </section>
-  )
+class Message extends React.Component{
+    //the render methods describes what you want to render in the DOM
+    render(){   
+        console.log(this.props)
+        return(
+            <div>
+                <h1 style={{color:this.props.color}}>
+                    {this.props.msg}
+                </h1>
+                <p>{this.props.age}</p>
+            </div>
+        )
+    }
 }
-const Library = ({books}) => {
-  return(
-    <div>
-      {books.map(
-        (book, i) =>  <Book
-                        key = {i}
-                        title={book.title}
-                        author={book.author}
-                        pages={book.pages}/>
-      )}
-    </div>
-  )
+var style = {
+    backgroundColor: 'orange',
+    color: 'white',
+    fontFamily: 'Arial'
 }
-
-render(
-  <Library books={bookList}/>,
-  document.getElementById('root')
-)
-
-
-
-//////---------Part 1
-/*
-let skiData = {
-  total:50,
-  powder: 20,
-  backcountry:10,
-  goal: 100
-}
-
-//-----SkyDayCounter as a function
-//This is a normal JS syntax
-const getPercent = decimal => {
-  return decimal * 100 + '%'
-}
-const calcGoalProgress = (total, goal) => {
-  return getPercent(total/goal)
-}
-const SkiDayCounter = ({total, powder, backcountry, goal}) => {
-  return (
-    <section>
-      <div>
-        <p>Total days: {total}</p>
-      </div>
-      <div>
-        <p>Powder days: {powder}</p>
-      </div>
-      <div>
-        <p>Backcountry days: {backcountry}</p>
-      </div>
-      <div>
-        <p>Goal Progress: {calcGoalProgress(total, goal)}</p>
-      </div>
-    </section>
-  )
-}
-//-----SkyDayCounter as a class
-// class SkyDayCounter extends Component {
-//
-//   getPercent = decimal => {
-//     return decimal * 100 + '%'
-//   }
-//   calcGoalProgress = (total, goal) => {
-//     return this.getPercent(total/goal)
-//   }
-//
-//   render(){
-//     const {total, powder, backcountry, goal} = this.props
-//     return (
-//
-//     )
-//   }
-// }
-
-
-class Message extends React.Component {
-  render(){
-    console.log(this.props);
-    return (
-      <div>
-        <h1 style = {{color:this.props.color}}>
-          {this.props.msg}
-        </h1>
-        <p>I'll check back in {this.props.minutes}</p>
-      </div>
+const title = React.createElement(
+    'ul',
+    {id:'title', className: 'header', style: style},
+    React.createElement(
+        'li',
+        {},
+        'item on the list'
     )
-  }
-}
-
-
-
-
-
-
-  render(
-  //<Message color= "blue" msg="how are you?" minutes={60}/>,
-  <SkiDayCounter
-    total={skiData.total}
-    powder={skiData.powder}
-    backcountry={skiData.backcountry}
-    goal={skiData.goal}
-  />,
-  document.getElementById('root')
 )
-*/
+
+ReactDOM.render(
+    <Message msg="How are you?" age={50} color="blue"/>, 
+    document.getElementById('root')
+)
