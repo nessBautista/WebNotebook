7cf625379dfaeda6c53d578e8cae312c20b60e33
Nestor Hernandez <nestor.jbautista@gmail.com>
2019-02-10 20:16:33 -0600

    Implementation of basic component with props

--- React102/index.js
+++ React102/index.js
@@ -1,1 +1,7 @@
-console.log('hola mundo!')
+import React from 'react';
+import {render} from 'react-dom';
+import Media from './src/playlist/components/media'
+
+const app = document.getElementById('app');
+const holaMundo = <h1>Hola mundo!!</h1>;
+render(<Media type="video" title="¿Que es responsive Design?" author="Ness" image="./images/covers/responsive.jpg"/>, app);
