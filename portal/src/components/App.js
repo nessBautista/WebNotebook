3cebe03984be1d0e7f1207d02872ae778285caf1
Ness Bautista <nestorjavierhernandez@Nestors-MacBook-Pro.local>
2019-06-27 22:17:55 -0500

    another test project with react

---
+++ portal/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import News from './News/News'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"> 
+       <h1>My feed</h1>           
+      </header>
+      <News />
+    </div>
+  );
+}
+
+export default App;
